5f2f29ab7b0cb32bf7f346ebbae23bc4f076c0fc
Mohmn <naqashmohmin1@gmail.com>
2021-04-12 18:31:26 +0530

    trying to fix a bug

--- covid_api/covid.js
+++ covid_api/covid.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     GlobalData()
+    const countries = {type:"country"},abbrevations = {type:"ab"},continents = {type:"continent"}
+    data(countries,abbrevations,continents)
+    
+    
+    
+    // document.getElementsByTagName('button')[0].addEventListener('click', function () {
 
-    document.getElementsByTagName('button')[0].addEventListener('click', function () {
+    //     const data = checkData()
 
-        const data = checkData()
+    //     if (data) {
+    //         particularData(data[0], data[1])
+    //     }
+    // })
 
-        if (data) {
-            particularData(data[0], data[1])
-        }
+    document.getElementsByTagName('input')[0].addEventListener('input',function(e){
+
+
+            let str = this.value.toLowerCase(),prev_str = null 
+            if(str.match(/^[A-Za-z0-9]+$/) && (str !== prev_str)){
+                console.log(str)
+
+                setTimeout(matching_results.bind({countries,abbrevations,continents,str}),1500)
+                prev_str = str
+            }
+
     })
 })
 
@@ -34,88 +51,125 @@ async function GlobalData() {
     }
 }
 
-
-function checkData() {
-
-    const selectBox = document.getElementsByTagName('Select')[0]
-    const number = Number(selectBox.value)
-    if (!number) {
-        selectBox.nextElementSibling.style.display = "block"
-        return false
-    }
-
-    let input = document.getElementsByTagName('input')[0].value
-    if (input.length === 0)
-        return false
-
-    selectBox.nextElementSibling.style.display = "none"
-
-    const type = selectBox.options[number].innerText.toLowerCase()
-    input = input[0].toUpperCase() + input.slice(1)
-    return [type, input]
-}
-
-async function particularData(type, place) {
+// made this function to minimize api calls
+async function data(countries,abbrevations,continents) {
 
     try {
+        const data = await fetch("https://covid-api.mmediagroup.fr/v1/cases")
 
-        console.log(`https://covid-api.mmediagroup.fr/v1/cases?${type}=${place}`)
-        const data = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?${type}=${place}`)
+        if (!data.ok) throw new Error("something went wrong reload page")
 
-        const response = await data.json()
+        const countries_data = await data.json()
+    
+ 
+        for(const cntr in countries_data){
+            
+            // has some undefined values for country ,abbreviation,continent
+            countries_data[cntr]["All"]["country"]
+            const country     = countries_data[cntr]["All"]["country"] ?? cntr.toLowerCase()
+            countries[country.toLowerCase()] = true
 
-        if (type === "continent") {
+            // otherwise many contries can have same "abbreviations"
+            if(!countries_data[cntr]["All"]['abbreviation'])continue
 
-            if (Object.keys(response).length === 0)
-                throw new Error("couldn't find the place")
+            const abbrevation = countries_data[cntr]["All"]['abbreviation']
+            abbrevations[abbrevation.toLowerCase()] = true
 
-            let confirmed = 0,
-                recovered = 0,
-                deaths = 0
-            for (const country in response) {
-
-                confirmed += response[country]["All"]["confirmed"]
-                deaths += response[country]["All"]["deaths"]
-                recovered += response[country]["All"]["recovered"]
-            }
-
-            const active = confirmed - (deaths + recovered)
-            showData([confirmed, recovered, active])
-
-        } else {
-            if (!response['All']['confirmed'])
-                throw new Error("couldn't find the place")
-
-            const confirmed = response["All"]["confirmed"]
-            deaths = response["All"]["deaths"]
-            recovered = response["All"]["recovered"]
-
-            const active = confirmed - (deaths + recovered)
-            showData([confirmed, recovered, active])
+            const continent = countries_data[cntr]["All"]['continent'] ?? "asia"
+            continents[continent.toLowerCase()] = true
         }
 
     } catch (err) {
 
-        const input = document.getElementsByTagName('input')[0]
-        input.nextElementSibling.style.display = "block"
-
-        const list = document.getElementsByClassName("list-group")[0]
-        list.style.display = "none"
+        console.error(`${err}`)
     }
 }
 
 
-function showData(data) {
+function showData(type,place,active,confirmed,recovered) {
 
     const list = document.getElementsByClassName("list-group")[0]
-    console.log(data,list)
-    for (let i = 0; i < list.childElementCount; i++) {
-        console.log( list[i])
-        list.children[i].children[0].innerText = data[i]
-    }
+    const dataElements = document.getElementsByClassName("data-elements")[0]
+    const li = document.createElement('li')
+    li.className = "list-group-item"
+    li.innerText = `${type} ${place}  ,Active cases ${active},Confirmed cases ${confirmed},recovered cases ${recovered}`
+    dataElements.appendChild(li)
 
     list.style.display = "block"
-
     const input = document.getElementsByTagName('input')[0]
     input.nextElementSibling.style.display = "none"
+}
+
+async function addData(matches,type) {
+    const list = document.getElementsByClassName("list-group")[0]
+    
+    // clear previious search results
+    if(list.childElementCount > 0){
+        console.log(list.children[0])
+        list.removeChild(list.children[0])
+    }
+    const dataElements =  document.createElement('div')
+
+    dataElements.className = "data-elements"
+    list.appendChild(dataElements)
+
+    if (type === "continent"){
+        console.log("continent",matches)
+        let place = matches[0]
+        // other wise gives wrong results
+        place = place[0].toUpperCase() + place.slice(1)
+        const data = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?${type}=${place}`)
+        const response = await data.json()
+
+        let confirmed = 0,recovered = 0,deaths = 0
+
+        for (const country in response) {
+            confirmed += response[country]["All"]["confirmed"]
+            deaths += response[country]["All"]["deaths"]
+            recovered += response[country]["All"]["recovered"]
+        }
+
+        const active = confirmed - (deaths + recovered)
+
+        showData(type,place,active,confirmed,recovered)
+    }
+    else{
+        for(let place of matches){
+            // other wise gives wrong results
+            place = place[0].toUpperCase() + place.slice(1)
+            const data = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?${type}=${place}`)
+            const response = await data.json()
+            console.log(place,type,response['All'])
+            const confirmed = response["All"]["confirmed"]
+            const deaths = response["All"]["deaths"]
+            const recovered = response["All"]["recovered"]
+
+            const active = confirmed - (deaths + recovered)
+            showData(type,place,active,confirmed,recovered)
+        }
+    }
+}
+
+
+
+function matching_results(){
+
+    if(this.abbrevations[this.str]){
+
+        addData([this.str],this.abbrevations['type'])
+        return 
+    }
+    else if(this.continents[this.str]){
+        addData([this.str],this.continents['type'])
+        return
+    }
+
+    let matches = []
+
+    for(let country in  this.countries){
+
+        if(country.includes(this.str))
+            matches.push(country)
+    }
+    addData(matches,this.countries['type'])
 }
